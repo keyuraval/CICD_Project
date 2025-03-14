7225d261de41ee7bd95a6712e9eb969debe8a2c3
Ubuntu <ubuntu@ip-172-31-90-222.ec2.internal>
2025-03-14 15:15:56 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World! This is our CI/CD pipeline demo.");
+});
+
